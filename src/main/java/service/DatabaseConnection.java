8cd0ee2112a74c85265770764a0485c6b9108128
VoTanThanhlaptrinh <votanthanh32004@gmail.com>
2024-12-11 16:48:49 +0700

    connection pool

--- src/main/java/service/DatabaseConnection.java
+++ src/main/java/service/DatabaseConnection.java
@@ -1,22 +1,53 @@
 package service;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConnection {
-	private static String driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	private static String url = "jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=book_store;encrypt=true;trustServerCertificate=true";
-	private static String username = "user_login";
-	private static String password = "123";
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
-	public static Connection getConnection() throws SQLException {
-		try {
-			Class.forName(driverClass);
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-		return DriverManager.getConnection(url, username, password);
-	}
+public class DatabaseConnection {
+    private static final String DRIVER_CLASS = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private static final String URL = "jdbc:sqlserver://localhost\\SQLEXPRESS;databaseName=book_store;encrypt=true;trustServerCertificate=true";
+    private static final String USERNAME = "user_login";
+    private static final String PASSWORD = "123";
+    private static final int MAX_POOL_SIZE = 10;
+    private static final int MIN_IDLE = 5;
+    private static final long CONNECTION_TIMEOUT = 10000;
+    private static final long IDLE_TIMEOUT = 600000;
+    private static final long MAX_LIFETIME = 1800000;
+
+    private static HikariDataSource dataSource;
+
+    public static synchronized Connection getConnection() throws SQLException {
+        if (dataSource == null) {
+            initializeDataSource();
+        }
+        return dataSource.getConnection();
+    }
+
+    private static synchronized void initializeDataSource() {
+        try {
+            HikariConfig config = new HikariConfig();
+            config.setDriverClassName(DRIVER_CLASS);
+            config.setJdbcUrl(URL);
+            config.setUsername(USERNAME);
+            config.setPassword(PASSWORD);
+            config.setMaximumPoolSize(MAX_POOL_SIZE);
+            config.setMinimumIdle(MIN_IDLE);
+            config.setConnectionTimeout(CONNECTION_TIMEOUT);
+            config.setIdleTimeout(IDLE_TIMEOUT);
+            config.setMaxLifetime(MAX_LIFETIME);
+
+            dataSource = new HikariDataSource(config);
+        } catch (Exception e) {
+            throw new RuntimeException("Error initializing DataSource", e);
+        }
+    }
+
+    public static synchronized void closeDataSource() {
+        if (dataSource != null) {
+            dataSource.close();
+        }
+    }
 }
